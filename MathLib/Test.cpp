ff570bea7f1320ff17ecacd6d80168cee2f98c8d
matthew.dowling <mjd117@gmail.com>
2016-09-26 11:28:08 -0700

    added test quad function

--- MathLib/Test.cpp
+++ MathLib/Test.cpp
@@ -1,5 +1,11 @@
+#include "Test.h"
 
 int doNothing(int x)
 {
 	return x*x + 2*x + 5;
+}
+
+float test_quad(float x)
+{
+	return x*x +2*x - 7;
 }
--- MathTests/main.cpp
+++ MathTests/main.cpp
@@ -4,9 +4,14 @@
 // what the flim flam
 int main()
 {
-	printf("Hello World! %d", doNothing(40));
+	// unit tests
+	assert(doNothing(0) == 5);
+	assert(doNothing(-1) == 4);
+	assert(doNothing(1) == 8);
 
-
-	getchar();
+	assert(test_quad(0.f) == -7);
+	assert(test_quad(1) == -4);
+	assert(test_quad(-1) == -8);
+	
 	return 0;
 }
